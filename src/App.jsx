7effb5aee8507a653cec57eac192163f2cbb5dc5
udip-rai <atomicdeveloper0329@gmail.com>
2024-02-21 16:17:09 +0545

    added counter logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useStore } from "./hooks/useBear";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { bears, increasePopulation, decreasePopulation } = useStore();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>{bears} around here...</h1>
+      <div style={{ display: "flex", gap: 10, justifyContent: "center" }}>
+        <button onClick={increasePopulation}>one up</button>
+        <button onClick={decreasePopulation}>one down</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
